4cfa26f79b05b2b4fb359146efc1fdc5589a1798
Pritam <pritamkalgekar@gmail.com>
2021-09-22 17:24:31 +0530

    cors

--- ztest_mode1/settings.py
+++ ztest_mode1/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders'
 ]
-#CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'ztest_mode1.urls'
 
 TEMPLATES = [
