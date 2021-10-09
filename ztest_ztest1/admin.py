74c82763947c643d268b7e575e507e89fa312991
Pritam <pritamkalgekar@gmail.com>
2021-10-09 10:14:57 +0530

    latest changes

--- ztest_ztest1/admin.py
+++ ztest_ztest1/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import cisco_output,ipaddress
+from .models import cisco_output,ipaddress,hostDetails
 
 admin.site.register(cisco_output)
 admin.site.register(ipaddress)
+admin.site.register(hostDetails)
